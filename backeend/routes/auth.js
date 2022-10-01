ea6c96dbd80738bb09bbfdbf36a7389906591a43
diego <contact@fartweb.com>
2022-10-01 11:20:25 -0300

    subiendo login and register username=mail

---
+++ backeend/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const Authcontroller = require("../controllers/AuthController");
+
+router.post("/register", Authcontroller.register);
+router.post("/login", Authcontroller.login);
+
+module.exports = router;
